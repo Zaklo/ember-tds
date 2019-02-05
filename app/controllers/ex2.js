6db54f18deb4848202da338eef7f81ec9c5f26be
Zaklo <44679226+Zaklo@users.noreply.github.com>
2019-02-05 21:15:03 +0100

    Add files via upload

--- app/controllers/ex2.js
+++ app/controllers/ex2.js
@@ -4,7 +4,7 @@ import Ember from 'ember';
 export default Controller.extend({
   actions: {
     toggleActive(service) {
-      Ember.set(service, 'active', Ember.get(service, 'active'));
+     service.set(active = true);
     }
   }
 });
