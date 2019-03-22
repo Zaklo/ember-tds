7b80aaaa46f751133ba82378613d4f6395d5c5f5
Zaklo <44679226+Zaklo@users.noreply.github.com>
2019-03-22 16:22:39 +0100

    Add files via upload
    avancement du TD4

---
+++ app/models/project.js
@@ -0,0 +1,9 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  name: DS.attr('string'),
+  descriptif: DS.attr('string'),
+  creationDate: DS.attr('date'),
+  duedate: DS.attr('date'),
+  owner: DS.belongsTo('developer')
+});
